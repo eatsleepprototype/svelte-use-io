29f746b544c1bdd5ae11f6fea02c9f8d15e74bf9
Derek Nguyen <dikanguok@gmail.com>
2022-02-28 12:06:16 +0900

    fix: wrong args in build script

--- src/lib/observer.ts
+++ src/lib/observer.ts
@@ -1,5 +1,5 @@
 import type { Options, ActionOptions } from './types';
-import { create_pointer } from '~/lib/create_pointer';
+import { create_pointer } from './create_pointer';
 
 export const default_callback: Options['callback'] = ({ entry, observer }) => {
 	const el = entry.target as HTMLElement;
